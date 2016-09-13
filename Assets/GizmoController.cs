fe794f562d740cde301d7d061dba82ee41d57a7a
sposito <sposito.thiago@gmail.com>
2016-09-13 10:29:24 -0300

    Basic Asset PlaceHolder

---
+++ Assets/GizmoController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GizmoController : MonoBehaviour {
+	public GameObject gizmdo;
+	public float rotationSpeed = 1f;
+	public float translateSpeed = 1f;
+	Vector3 movement;
+	// Use this for initialization
+	void Start () {
+		movement = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		gizmdo.transform.Rotate(Vector3.up * rotationSpeed);
+		movement = translateSpeed/100 * Vector3.up * Mathf.Sin (gizmdo.transform.rotation.eulerAngles.y * Mathf.Deg2Rad);
+		gizmdo.transform.Translate (movement);
+	}
+}
