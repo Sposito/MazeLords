cfc0c0f43969ef0bdff4bd5235502ec2694307f9
Sposito <sposito.thiago@gmail.com>
2016-11-21 01:38:46 -0200

    Login graphic setup
    New LoginMenu Initial graphic setup

---
+++ Assets/Scripts/Viewer/IslandsOscilate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IslandsOscilate : MonoBehaviour {
+
+	public AnimationCurve islandAnimation;
+	public float speed = 1f;
+	public float amount = 1f;
+
+	void Start () {
+		speed += Random.Range (-20f, 20f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate( (amount * Vector3.up * islandAnimation.Evaluate(speed * Time.time )));
+	}
+}
